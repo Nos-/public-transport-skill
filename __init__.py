d028cadde1e089655772ada8a1db14a0e0ac32e3
Nos- <Nos-@users.noreply.github.com>
2019-11-10 01:12:24 +0100

    Debugging Python-Einrückungen, Intents und Antworten

--- __init__.py
+++ __init__.py
@@ -2,14 +2,20 @@ from mycroft import MycroftSkill, intent_file_handler
 
 
 class PublicTransport(MycroftSkill):
-    def __init__(self):
-        MycroftSkill.__init__(self)
+	def __init__(self):
+		MycroftSkill.__init__(self)
 
-    @intent_file_handler('transport.public.intent')
-    def handle_transport_public(self, message):
-        self.speak_dialog('transport.public')
-
+	@intent_file_handler('transport.public.intent')
+	def handle_transport_public(self, message):
+		startort_type = message.data.get('StartOrt')
+		zielort_type = message.data.get('ZielOrt')
+		linie_type = message.data.get('Linie')
+		if zielort_type is not None:
+			self.speak_dialog('transport.public',
+							{'StartOrt': startort_type, 'ZielOrt': zielort_type})
+		else:
+			self.speak_dialog('transport.public.generic')
 
 def create_skill():
-    return PublicTransport()
-
+	return PublicTransport()
+	
